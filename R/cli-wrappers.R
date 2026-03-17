5be443d69683c4491c1325ef018bb92f51ff9598
elena-iri <s243312@dtu.dk>
2026-03-17 16:08:28 +0100

    fixed cli wrappers for test-install.R to run. Also deleted readme explaining tests

--- R/cli-wrappers.R
+++ R/cli-wrappers.R
@@ -1,25 +1,25 @@
 # CLI wrapper helpers (to make messaging easy to mock in tests) ──────────────
 
 .cli_h1 <- function(...) {
-  cli::cli_h1(...)
+  cli::cli_h1(..., .envir = parent.frame())
 }
 
 .cli_bullets <- function(...) {
-  cli::cli_bullets(...)
+  cli::cli_bullets(..., .envir = parent.frame())
 }
 
 .cli_alert_info <- function(...) {
-  cli::cli_alert_info(...)
+  cli::cli_alert_info(..., .envir = parent.frame())
 }
 
 .cli_alert_success <- function(...) {
-  cli::cli_alert_success(...)
+  cli::cli_alert_success(..., .envir = parent.frame())
 }
 
 .cli_alert_warning <- function(...) {
-  cli::cli_alert_warning(...)
+  cli::cli_alert_warning(..., .envir = parent.frame())
 }
 
 .cli_alert_danger <- function(...) {
-  cli::cli_alert_danger(...)
+  cli::cli_alert_danger(..., .envir = parent.frame())
 }
